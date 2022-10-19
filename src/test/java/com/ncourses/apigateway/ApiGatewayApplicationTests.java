9ff50556ead39dcca08318f78505e1004dd427ae
tnicacio <nicacio.t92@gmail.com>
2022-10-18 23:06:25 -0300

    Adding initial configuration

---
+++ src/test/java/com/ncourses/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncourses.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
